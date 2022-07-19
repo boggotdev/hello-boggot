497e72daf3ba87d04c118e4e31f5e1ad33d1b32b
boggt <boggot.dev@gmail.com>
2022-07-19 13:05:24 +0530

    pages data changed to Lorem Ipsum

--- src/views/Services.js
+++ src/views/Services.js
@@ -5,16 +5,16 @@ import FeaturesTiles from '../components/sections/FeaturesTiles';
 import FeaturesSplit from '../components/sections/FeaturesSplit';
 import Testimonial from '../components/sections/Testimonial';
 import Cta from '../components/sections/Cta';
+import './dummy.css';
 
 const Services = () => {
 
   return (
     <>
-      <Hero className="illustration-section-01" />
-      <FeaturesTiles />
-      <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
-      <Testimonial topDivider />
-      <Cta split />
+     <div className="dummy">
+      <h3>Services</h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit delectus corporis omnis inventore ullam fugit blanditiis velit veritatis, esse sunt hic autem officia laudantium in temporibus, aperiam placeat reiciendis voluptatem!</p>
+    </div>
     </>
   );
 }
